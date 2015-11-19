bac3f1f7058a91e69c48fa3aca11b8cf41544cde
Alex MacCaw <alex@clearbit.com>
2015-11-19 09:40:52 -0800

    make sure Mash::Camel returns more Mash Camels

--- lib/mash/camel.rb
+++ lib/mash/camel.rb
@@ -4,4 +4,18 @@ class Mash::Camel < Mash
   def convert_key(key)
     key.to_s.underscore
   end
+
+  def convert_value(val, duping=false) #:nodoc:
+    case val
+      when self.class
+        val.dup
+      when ::Hash
+        val = val.dup if duping
+        Mash::Camel.new(val)
+      when ::Array
+        val.map {|e| convert_value(e) }
+      else
+        val
+    end
+  end
 end
